f47257f24e0452adab8bec45d396601490a5e858
mercuryyujia <yf2148@nyu.edu>
2024-10-25 20:33:37 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Charts from "./assignment4_YujiaFu"; // Import the Charts component
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Citi Bike Data Visualization</title>
+        <meta name="description" content="InfoVisHW4_Yujia" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main style={{ margin: 0, padding: 0, backgroundColor: "white" }}>
+        {/* Render the Charts component containing the scatter plot */}
+        <Charts />
+      </main>
+    </>
+  );
+}
